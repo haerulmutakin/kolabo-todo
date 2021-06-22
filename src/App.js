31fc95a6ff84f99b70cdd1663ca1edff436c38fb
haerulmutakin <haerulm.takin@gmail.com>
2021-06-22 21:15:13 +0700

    wrapp component with AuthProvider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.scss';
 import Login from './component/login/Login';
+// import History from './_helpers/CustomHistory';
+import Core from './component/core/Core';
+import RouteGuard from './_helpers/RouteGuard';
 import AuthProvider from './provider/AuthProvider';
 
 function App() {
   return (
     <AuthProvider>
-        <Login />
+      <Router>
+          <RouteGuard exact path="/" component={Core} />
+          <Route exact path="/login" component={Login} />
+      </Router>
     </AuthProvider>
   );
 }
