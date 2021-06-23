e7de1dbba7ee3161cdfbef668279f42935b5bd63
haerulmutakin <haerulm.takin@gmail.com>
2021-06-23 22:45:13 +0700

    change edit ui

--- src/component/task/Task.js
+++ src/component/task/Task.js
@@ -4,13 +4,12 @@ import firebaseDB from '../../_firebase-conf/firebase.config';
 
 
 const Task = ({
-    taskEditable = true,
     taskData
 }) => {
-    const tasksRef = firebaseDB.firestore().collection('coba');
+    const tasksRef = firebaseDB.firestore().collection('tasks');
     const [ task, setTask ] = useState([]);
-    const [ taskEditMode, setTaskEditMode ] = useState(false);
     const [ editableTaskValue, setEditableTaskValue ] = useState('');
+    const [modal, setModal] = useState(false);
 
     useEffect(() => {
         setTask(taskData);
@@ -18,7 +17,7 @@ const Task = ({
 
     const handleActionEditTask = (e) => {
         setEditableTaskValue(taskData.title);
-        setTaskEditMode(true);
+        setModal(true);
     }
 
     const handleTaskUpdateKeyDown = (e, item) => {
@@ -26,7 +25,7 @@ const Task = ({
             e.preventDefault();
             item.title = e.target.value;
             updateTodo(item);
-            setTaskEditMode(false);
+            setModal(false);
           }
     }
 
@@ -39,25 +38,45 @@ const Task = ({
             })
     }
 
-    return (
-        <div className="all-container">
-            {taskEditMode ? 
-                <textarea 
-                    autoFocus 
-                    placeholder="Enter task name..." 
-                    value={editableTaskValue} 
-                    className="task-input" 
-                    onChange={(e) => setEditableTaskValue(e.target.value)} 
-                    onKeyDown={(e) => handleTaskUpdateKeyDown(e, taskData)} 
-                ></textarea>
-                : 
-                <div className="task-container">
-                    {task.title}
-                    {taskEditable && <Button onClick={handleActionEditTask}>Edit</Button>}
-                </div> 
+    const handleCancel = (item) => {
+        item.status = 3;
+        updateTodo(item);
+        setModal(false);
+    }
 
-            }
-        </div>
+    const hadnleNextStep = (item) => {
+        if (item.status < 3) {
+            item.status += 1;
+            updateTodo(item);
+            setModal(false);
+        }
+    }
+
+    return (
+        <div className="task-container">
+            <div className="task-detail" onClick={handleActionEditTask}>
+                {task.title}
+            </div>
+            {modal && 
+                <div className="coba-modal">
+                    <div className="edit-modal">
+                        <textarea 
+                            autoFocus 
+                            placeholder="Enter task name..." 
+                            value={editableTaskValue} 
+                            className="task-input" 
+                            onChange={(e) => setEditableTaskValue(e.target.value)} 
+                            onKeyDown={(e) => handleTaskUpdateKeyDown(e, taskData)} 
+                        ></textarea>
+                        <div className="edit-action">
+                            <Button onClick={() => handleCancel(taskData)} size="sm" color="red">Cancel</Button>
+                            <Button onClick={() => hadnleNextStep(taskData)} size="sm" color="blue">Progress</Button>
+                        </div>
+                    </div>
+                    <div className="edit-backdrop" onClick={() => setModal(false)}></div>
+                </div>
+                }
+        </div> 
     )
 }
 
