77c616409e0f7641337453012c3bd56d2225cd2b
haerulmutakin <haerulm.takin@gmail.com>
2021-06-23 11:36:02 +0700

    add borad and task

--- src/component/core/Core.js
+++ src/component/core/Core.js
@@ -1,22 +1,93 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../../_provider/AuthProvider';
-import { auth } from '../../_firebase-conf/firebase.config';
+import React, {useState } from 'react';
+import { Grid, Col, Icon, IconButton } from 'rsuite';
+import Header from '../common/Header';
 
 const Core = () => {
-    const user = useContext(AuthContext);
-    console.log(user);
-    const doLogout = (e) => {
+    const [newTask, setNewTask] = useState('');
+    const [showNewTask, setShowNewTask] = useState(false);
+    const [task, setTask] = useState(
+        []
+    );
+
+    const onEnterPress = (e) => {
+        if(e.keyCode === 13 && e.shiftKey === false) {
+          e.preventDefault();
+          const newEl = {
+              title: e.target.value
+          }
+          setTask([...task, newEl]);
+          setNewTask('');
+        }
+    }
+
+    const newTasHandler = (e) => {
         e.preventDefault();
-        auth.signOut().then(res => {
-            console.log(res);
-        }).catch(err => {
-            console.log(err);
-        })
+        setShowNewTask(true);
     }
     return (
         <div>
-            <p>Ini adalah core</p>
-            <button onClick={doLogout}>Logout</button>
+            <Header />
+            <Grid fluid>
+                <Col className="board" md={6}>
+                    <div className="board-header">
+                        <div className="board-title-container">
+                            <p>TODO</p>
+                        </div>
+                        <div onClick={newTasHandler}><Icon icon="data-increase" /></div>
+                    </div>
+                    <div className="board-content">
+                        {task.map((item, index) => {
+                            return <div key={index} className="task-container">
+                                {item.title}
+                            </div>
+                        })}
+                        {showNewTask && 
+                        <div className="task-input-container">
+                            <textarea 
+                                autoFocus 
+                                placeholder="Enter task name..." 
+                                value={newTask} 
+                                className="task-input" 
+                                onChange={(e) => setNewTask(e.target.value)} 
+                                onKeyDown={onEnterPress} 
+                            ></textarea>
+                        </div>
+                        }
+                        <div className="add-task-container">
+                            <IconButton onClick={newTasHandler} icon={<Icon icon="data-increase" />} placement="left">
+                                Add New Task
+                            </IconButton>
+                        </div>
+                    </div>
+                </Col>
+                <Col className="board" md={6}>
+                    <div className="board-header">
+                        <div className="board-title-container">
+                            <p>PROGRESS</p>
+                        </div>
+                    </div>
+                    <div className="board-content">
+                    </div>
+                </Col>
+                <Col className="board" md={6}>
+                    <div className="board-header">
+                        <div className="board-title-container">
+                            <p>DONE</p>
+                        </div>
+                    </div>
+                    <div className="board-content">
+                    </div>
+                </Col>
+                <Col className="board" md={6}>
+                    <div className="board-header">
+                        <div className="board-title-container">
+                            <p>CANCELED</p>
+                        </div>
+                    </div>
+                    <div className="board-content">
+                    </div>
+                </Col>
+            </Grid>
         </div>
     )
 }
